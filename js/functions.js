9907eb7279ee47738f26e8f3ce449150f34bc5e4
Александр П <un7uckj@gmail.com>
2024-10-23 01:23:10 +0500

    Ещё подправил функции

--- js/functions.js
+++ js/functions.js
@@ -22,3 +22,20 @@ const getDigits = (input) => {
 
   return parseInt(result, 10);
 };
+
+
+checkStringLength('проверяемая строка', 20);
+checkStringLength('проверяемая строка', 18);
+checkStringLength('проверяемая строка', 10);
+checkPalindrome('топот');
+checkPalindrome('ДовОд');
+checkPalindrome('Кекс');
+checkPalindrome('Лёша на полке клопа нашёл ');
+getDigits('2023 год');
+getDigits('ECMAScript 2022');
+getDigits('1 кефир, 0.5 батона');
+getDigits('агент 007');
+getDigits('а я томат');
+getDigits(2023);
+getDigits(-1);
+getDigits(1.5);
